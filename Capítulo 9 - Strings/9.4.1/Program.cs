eb089ef84d2a19eab4c775b79314f8962125104d
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-07-25 10:41:34 -0300

    Capitulo 11

---
+++ Capítulo 9 - Strings/9.4.1/Program.cs
@@ -0,0 +1,4 @@
+﻿string texto1 = "Olá";
+string texto2 = "Olá";
+bool saoIguais = (texto1 == texto2);
+Console.WriteLine(saoIguais);
---
+++ Capítulo 9 - Strings/Desafios/5/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Escreva um programa que solicite ao usuário que digite uma frase. Em seguida, realize as seguintes operações:
+•	Imprima o número total de caracteres na frase.
+
+•	Converta a frase para letras maiúsculas e imprima-a.
+
+•	Verifique se a frase contém a palavra "exemplo" e imprima o resultado.
+
+*/
+
+/**************************Solução abaixo*********************************/
+
+Console.WriteLine("Digite uma frase:");
+string frase = Console.ReadLine(); // Lê a frase digitada pelo usuário.
+
+// Imprime o número total de caracteres na frase.
+Console.WriteLine($"Número total de caracteres na frase: {frase.Length}");
+
+// Converte a frase para letras maiúsculas e imprime-a.
+string fraseMaiusculas = frase.ToUpper();
+Console.WriteLine($"Frase em letras maiúsculas: {fraseMaiusculas}");
+
+// Verifica se a frase contém a palavra "exemplo" e imprime o resultado.
+bool contemExemplo = fraseMaiusculas.Contains("EXEMPLO");
+Console.WriteLine($"A frase {(contemExemplo ? "contém" : "não contém")} a palavra (exemplo).");
