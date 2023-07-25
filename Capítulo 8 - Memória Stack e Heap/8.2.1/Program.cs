99a6f10c7bc69d8b8c0f710a5f5bd2e6d01c62b4
BrunoBafilli <134939811+CodigoDasCoisas@users.noreply.github.com>
2023-07-25 12:18:11 -0300

    Livro

---
+++ Capítulo 8 - Memória Stack e Heap/8.2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Alocação de vetores na heap
+int[] array = new int[5];
+string[] nomes = new string[3];
+// Uso dos vetores alocados na heap
+array[0] = 42;
+nomes[0] = "João";
+nomes[1] = "Maria";
+// Impressão dos valores
+Console.WriteLine(array[0]);
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+// Liberação de memória
+array = null;
+nomes = null;
---
+++ Capítulo 8 - Memória Stack e Heap/8.2.2/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] vetor1 = { 1, 2, 3 }; // Cria um vetor
+int[] vetor2 = vetor1;
+vetor2[0] = 10;
+Console.WriteLine(vetor1[0]);
